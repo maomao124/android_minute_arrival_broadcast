d0355e8512b9db2346f6b6941dfc878f903d8b9b
mao <1296193245@qq.com>
2022-10-10 10:58:46 +0800

    android_minute_arrival_broadcast

--- app/src/main/java/mao/android_minute_arrival_broadcast/MainActivity.java
+++ app/src/main/java/mao/android_minute_arrival_broadcast/MainActivity.java
@@ -2,15 +2,38 @@ package mao.android_minute_arrival_broadcast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+
+import mao.android_minute_arrival_broadcast.receiver.MyReceiver;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private MyReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        receiver = new MyReceiver();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
